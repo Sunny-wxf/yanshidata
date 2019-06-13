3200048179bf256e42b4e6ab1b1c504ef2c655c2
sunny <976398860@qq.com>
2019-06-13 17:30:52 +0800

    实名认证完成

--- create_data.py
+++ create_data.py
@@ -1,13 +1,13 @@
 __author__ = 'Wangxiaofeng'
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding:utf-8 -*-
 
 import requests
-from urllib import parse
 import random
 from logger import Log
 from oracle import MyOracle
-import getdata
+from getdata import GetData as GetData
+import sys
 
 
 class CreateData(object):
@@ -20,34 +20,42 @@ class CreateData(object):
         :param self.url: 请求域名
         :param self.user_info: 请求参数
         """
-        self.url = 'http://39.105.191.175:8080/twweb'
+        self.url = 'http://192.168.3.186:8090/twweb'
         self.user_info = {'mobile': 14611110000, 'password': 'e10adc3949ba59abbe56e057f20f883e'}
         self.oracle = MyOracle()
-        self.name = getdata.get_name()
+        self.name = GetData.get_name()
+        self.id = GetData.generator()
 
     def real_name(self):
+        """
+        实名认证：读取数据库中未实名认证过的user_id,将user_id匹配随机姓名与身份证号通过接口生成
+        :return:null
+        """
+        self.__logging.debug('-----------------------------------------------------------------')
         url_real_name = self.url + '/RealNameAuthApplicationController_4M/insertRealNameAuthApplication.action'
-        name = self.name
-        user_id = random.sample(self.oracle.select_all('select id from user_info u where u.real_name_id is NULL'), 1)
-        form_register = {
-            'name':'',
-            'user_id': user_id[0][0],
-            'face_id': '%2F9j',
-            'id_end_time': '2024-06-10',
-            'alipay_user_id': '',
-            'pt_id_number': 110101199003071110,
-            'open_user': '',
+        user_id = random.sample(self.oracle.select_all('select id from user_info u where u.real_name_id is NULL'),
+                                int(sys.argv[1]))
+        for i in range(1, int(sys.argv[1])+1):
+            name = self.name[i]
+            form_register = {
+                'name': name,
+                'user_id': user_id[i-1][0],
+                'face_id': '%2F9j',
+                'id_end_time': '2024-06-10',
+                'alipay_user_id': '2088022880191155',
+                'pt_id_number': self.id[1],
+                'open_user': name,
+                'bank_id': '-1',
+                'open_bank': '-1 ',
+                'mobile': '-1 ',
+                'my_photo': '%2F9j'
+            }
+            r = requests.post(url_real_name, data=form_register)
+            if r.status_code == 200:
+                self.__logging.debug('序号' + str(i) + ':  ' + user_id[i-1][0] + '--' + name + '  实名认证成功')
+            else:
+                self.__logging.debug('The request error,please check')
 
-            'bank_id' = -1,
-
-                      'open_bank' = -1 &
-
-                                  mobile = -1 &
-
-                                           my_photo
-        }
-        r = requests.post(url_real_name, data=form_register)
-        print(r.text)
 
     def apply_provincial_agent(self):
         """
@@ -59,10 +67,7 @@ class CreateData(object):
         user_info1 = self.user_info.copy()
         user_info1['logintype'] = '2'
         r = requests.post(url_login, data=user_info1)
-        self.assertIn('登录成功', parse.unquote(parse.unquote(r.text)))
         return r.cookies
-
-
 
 
 if __name__ == '__main__':
--- getdata.py
+++ getdata.py
@@ -1,33 +1,66 @@
 import requests
 import re
-from xlwt import Workbook
 from conf import Config as Config
 import random
-# import xlrd
-# import time
+import datetime
 
 
-def get_name():
-    # 获取百家姓对应名字集的链接
-    url = Config().getconf("name").url_name
-    res = requests.get(url)
-    reg_last_name = 'href="//[A-Za-z]+\.'
-    reg_last_name = re.compile(reg_last_name)
-    last_name = re.findall(reg_last_name, res.text)
-    last_name_dic = []
-    for last_name in last_name:
-        url_last_name = 'http:' + last_name[6:len(last_name) - 1] + '.resgain.net/name_list.html'
-        last_name_dic.append(url_last_name)
-    # 获取百家姓中每个姓氏随机10个名字
-    name_dic = []
-    for link in last_name_dic:
-        res = requests.get(link)
-        reg_get_name = '(?<=href="/name/).*?(?=.html" class)'
-        reg_name = re.compile(reg_get_name)
-        name = re.findall(reg_name, res.text)
-        [name_dic.append(x) for x in random.sample(name, 4)]
-    print(name_dic)
+
+class GetData(object):
+    # 为生成数据获取各种类型的数据
+    def get_name(self):
+        """
+        姓名
+        :return: name_dic
+        """
+        # 获取百家姓对应名字集的链接
+        url = Config().getconf("name").url_name
+        res = requests.get(url)
+        reg_last_name = 'href="//[A-Za-z]+\.'
+        reg_last_name = re.compile(reg_last_name)
+        last_name = re.findall(reg_last_name, res.text)
+        last_name_dic = []
+        for last_name in last_name:
+            url_last_name = 'http:' + last_name[6:len(last_name) - 1] + '.resgain.net/name_list.html'
+            last_name_dic.append(url_last_name)
+        # 获取百家姓中每个姓氏随机10个名字
+        name_dic = []
+        for link in last_name_dic[:2]:
+            res = requests.get(link)
+            reg_get_name = '(?<=href="/name/).*?(?=.html" class)'
+            reg_name = re.compile(reg_get_name)
+            name = re.findall(reg_name, res.text)
+            [name_dic.append(x) for x in random.sample(name, 4)]
+        return name_dic
+
+    def generator(self):
+        """
+        生成随机身份证号
+        :return:id_list
+        """
+        id_list = []
+        with open('./area.csv', mode="r", encoding="utf-8") as file:
+            codelist = file.readlines()
+        num = 1
+        while num <= int(sys.argv[1]):
+            id_number = codelist[random.randint(0, len(codelist) - 1)].split(" ")[0]  # 地区项
+            id_number = id_number + str(random.randint(1980, 2019))  # 年份项
+            da = datetime.date.today() + datetime.timedelta(days=random.randint(1, 366))  # 月份和日期项
+            id_number = id_number + da.strftime('%m%d')
+            id_number = id_number + str(random.randint(100, 300))  # ，顺序号简单处理
+            i = 0
+            count = 0
+            weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]  # 权重项
+            checkcode = {'0': '1', '1': '0', '2': 'X', '3': '9', '4': '8', '5': '7', '6': '6', '7': '5',
+                         '8': '5', '9': '3', '10': '2'}  # 校验码映射
+            for i in range(0, len(id_number)):
+                count = count + int(id_number[i]) * weight[i]
+            id_number = id_number + checkcode[str(count % 11)]  # 算出校验码
+            id_list.append(id_number)
+            num += 1
+        return id_list
 
 
 if __name__ == '__main__':
-    get_name()
+    get_data = GetData()
+    get_data.get_name()
